3fae6c75f39669a4ed77103f6eca0ec32c8f1910
George Karanikas <george.karanikas@foodtecsolutions.com>
2018-12-12 07:31:23 -0500

    (feature) implement day 2 part 2

---
+++ src/test/java/day2/Day2Part2Test.java
@@ -0,0 +1,25 @@
+package day2;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class Day2Part2Test {
+
+    private final Day2Part2 oracle = new Day2Part2();
+
+    @Test
+    public void solve() {
+	assertEquals("fgij", oracle.solve(List.of(
+	    "abcde",
+	    "fghij",
+	    "klmno",
+	    "pqrst",
+	    "fguij",
+	    "axcye",
+	    "wvxyz"
+	)));
+    }
+}
