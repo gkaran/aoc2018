fd9330a8ac77aefa50d5828c8264b11c3fd8d38a
George Karanikas <george.karanikas@foodtecsolutions.com>
2018-12-13 10:46:12 -0500

    skeleton for day 4 part 1

---
+++ src/main/java/aoc/day4/Day4Part1.java
@@ -0,0 +1,12 @@
+package aoc.day4;
+
+import aoc.Solution;
+
+import java.util.List;
+
+public class Day4Part1 implements Solution<Integer> {
+    @Override
+    public Integer solve(List<String> data) {
+	return null;
+    }
+}
